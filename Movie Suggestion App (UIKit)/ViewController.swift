99b49c279e82e2e9b7a9088d64a6dc41123fb32f
Prem Gundu <60621227+prem2279@users.noreply.github.com>
2026-06-23 11:23:43 -0400

    Initial Commit

---
+++ Movie Suggestion App (UIKit)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie Suggestion App (UIKit)
+//
+//  Created by Prem Kumar Gundu on 6/23/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
